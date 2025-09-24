3345eed7e821e50a358c6935dd6cb2d7dbdc336c
SanjeetWD1419 <sanjeetkumar21354@gmail.com>
2025-09-24 20:10:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple contact form (demo only)
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Message Sent! (Demo Only)");
+});
